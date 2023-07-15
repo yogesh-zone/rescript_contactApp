4487267988f5075086d9418a524981386a487650
YogeshBalodi <Yogesh.b@antino.io>
2023-07-15 09:49:44 +0530

    basic template for react + rescript

---
+++ src/App.res
@@ -0,0 +1,6 @@
+%%raw(`import './App.css';`)
+
+@react.component
+let make = () => {
+  <div className="underline font-bold"> {"Hello Yogesh!"->React.string} </div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{res,js,jsx,tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
